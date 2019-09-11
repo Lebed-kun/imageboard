bde801cf6a509d8697e858d3e633ffa9b09a0757
Lebed-kun <lebed-kun@mail.ru>
2019-09-11 12:57:42 +0400

    Add auth  models

--- server/imageboard/models.py
+++ server/imageboard/models.py
@@ -7,16 +7,51 @@ from . import validators
 
 # Only technical admins can change data of these models
 
+class User(models.Model):
+    DEFAULT_GROUP = UserGroup.objects.get(name='User')
+    
+    name = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    pass_hash = models.CharField(max_length=100)
+    pass_salt = models.CharField(max_length=100)
+    pass_algo = models.CharField(choices=[])
+    group = models.ForeignKey('UserGroup', on_delete=models.SET_DEFAULT, related_name='user_groups', default=DEFAULT_GROUP)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class UserGroup(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    priveleges = models.ManyToManyField('Privelege', related_name='priveleges', null=True, blank=True)
+
+    def __name__(self):
+        return self.name
+
+    def delete(self, *args, **kwargs):
+        if self.name == 'User':
+            raise Exception('Cannot delete User group!')
+        super().delete(*args, **kwargs)
+
+class Privelege(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 # Public models
 
 class Board(models.Model):
     name = models.CharField(max_length=100, unique=True)
     abbr = models.CharField(max_length=10, unique=True, validators=[validators.abbr_validator])
-    description = models.TextField()
+    description = models.TextField(blank=True)
     bump_limit = models.IntegerField(default=500)
-    spam_words = models.CharField(validators=[validators.csv_validator])
-    picture = models.ImageField(upload_to="board_avas/")
-    # author
+    spam_words = models.CharField(blank=True, validators=[validators.csv_validator])
+    picture = models.ImageField(upload_to="board_avas/", null=True, blank=True)
+    author = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -25,21 +60,27 @@ class Board(models.Model):
 
 class Thread(models.Model):
     sticked = models.BooleanField(default=False)
-    got_bump_limit = models.BooleanField(default=False)
     read_only = models.BooleanField(default=False)
     first_post = models.OneToOneField('Post', on_delete=models.CASCADE, related_name='first_post', null=True, blank=True)
     board = models.ForeignKey('Board', on_delete=models.CASCADE, related_name='boards')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    posts_count = models.IntegerField(default=0)
 
     def __str__(self):
-        return ''
+        if self.first_post is not None:
+            return str(self.first_post)
+        else:
+            return str(self.id)
+
+    def has_bump_limit(self):
+        return self.posts_count >= self.board.bump_limit
 
 class Post(models.Model):
     title = models.CharField(max_length=200, blank=True)
     author = models.CharField(max_length=50, blank=True)
     contact = models.CharField(max_length=100, blank=True, validators=[EmailValidator, URLValidator])
-    options = models.CharField(validators=[validators.csv_validator])
+    options = models.CharField(blank=True, validators=[validators.csv_validator])
     message = models.TextField(max_length=15000)
     poster_ip = models.GenericIPAddressField()
     thread = models.ForeignKey('Thread', on_delete=models.CASCADE, related_name="threads")
@@ -52,6 +93,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if self.thread.first_post is None:
             self.thread.first_post = self
+        self.thread.posts_count += 1
         super().save(*args, **kwargs)   
 
 class PostFile(models.Model):
