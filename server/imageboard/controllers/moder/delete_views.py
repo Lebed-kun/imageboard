5251773309dc4f26e5b6e3e8c39262fe20a95a42
Lebed-kun <lebed-kun@mail.ru>
2019-10-16 15:09:39 +0400

    Add controller for deleting post

--- server/imageboard/controllers/moder/delete_views.py
+++ server/imageboard/controllers/moder/delete_views.py
@@ -61,3 +61,56 @@ def delete_report(request, id, *args, **kwargs):
                 return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
+
+def delete_post(request, id, *args, **kwargs):
+    if request.method == 'DELETE':
+        # Check if user is authorized
+        ip = kwargs.get('ip', get_visitor_ip(request))
+        if not is_user_authorized(ip):
+            message = {
+                'message' : 'User is not authorized.'
+            }
+            return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+        
+        # Check if user has access to delete posts
+        token = models.UserToken.objects.filter(ip=ip)[0]
+        user = models.User.objects.filter(token=token)[0]
+        user_priveleges = user.get_priveleges(priveleges.DELETE_POSTS)
+        if len(user_priveleges) == 0:
+            message = {
+                'message' : 'User doesn\'t have permission to delete posts.'
+            }
+            return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+
+        # Check if post exists
+        post = None
+        try:
+            post = models.Post.objects.get(id=id)
+        except models.Post.DoesNotExist:
+            message = {
+                'message' : 'Post doesn\'t exist.'
+            }
+            return Response(message, status=status.HTTP_404_NOT_FOUND, content_type='application/json')
+
+        # Success
+        if user_priveleges[0]['board'] is None:
+            post.delete()
+            message = {
+                'message' : 'Delete succeed.'
+            }
+            return Response(message, status=status.HTTP_204_NO_CONTENT, content_type='application/json')
+        else:
+            for priv in user_priveleges:
+                if priv['board'] == post.thread.board:
+                    post.delete()
+                    message = {
+                        'message' : 'Delete succeed.'
+                    }
+                    return Response(message, status=status.HTTP_204_NO_CONTENT, content_type='application/json')
+            else:
+                message = {
+                    'message' : 'User doesn\'t have permission to delete posts from board /{}/.'.format(report.board.abbr)
+                }
+                return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
--- server/imageboard/priveleges.py
+++ server/imageboard/priveleges.py
@@ -1,2 +1,4 @@
 GET_REPORTS = 'get_reports'
 DELETE_REPORTS = 'delete_reports'
+
+DELETE_POSTS = 'delete_posts'
