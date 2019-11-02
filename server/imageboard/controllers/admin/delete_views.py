6430c2ab4f41106bc21ebca6fd8a28533c458216
Lebed-kun <lebed-kun@mail.ru>
2019-11-02 14:18:51 +0400

    Implement delete board handler

---
+++ server/imageboard/controllers/admin/delete_views.py
@@ -0,0 +1,63 @@
+from rest_framework.response import Response
+from django.core.files.base import ContentFile
+from rest_framework import status
+from datetime import datetime, timezone
+
+from ... import models
+from ... import constants
+from ...utils import get_visitor_ip
+from ..user.post_views import is_user_authorized
+from ... import priveleges
+
+def delete_board(request, id, *args, **kwargs):
+    if request.method == 'DELETE':
+        # Check if user is authorized
+        ip = kwargs.get('ip', get_visitor_ip(request))
+        if not is_user_authorized(ip):
+            message = {
+                'message' : 'User is not authorized.'
+            }
+            return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+
+        # Check if user has access to delete boards
+        token = models.UserToken.objects.filter(ip=ip)[0]
+        user = models.User.objects.filter(token=token)[0]
+        user_priveleges = user.get_priveleges(priveleges.DELETE_BOARDS)
+        if len(user_priveleges) == 0:
+            message = {
+                'message' : 'User doesn\'t have permission to delete boards.'
+            }
+            return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+
+        # Check if board exists
+        board = None
+        try:
+            board = models.Board.objects.get(id=id)
+        except models.Report.DoesNotExist:
+            message = {
+                'message' : 'Board doesn\'t exist.'
+            }
+            return Response(message, status=status.HTTP_404_NOT_FOUND, content_type='application/json')
+
+        # Success
+        if user_priveleges[0]['board'] is None:
+            board.delete()
+            message = {
+                'message' : 'Delete succeed.'
+            }
+            return Response(message, status=status.HTTP_204_NO_CONTENT, content_type='application/json')
+        else:
+            for priv in user_priveleges:
+                if priv['board'] == board:
+                    board.delete()
+                    message = {
+                        'message' : 'Delete succeed.'
+                    }
+                    return Response(message, status=status.HTTP_204_NO_CONTENT, content_type='application/json')
+            else:
+                message = {
+                    'message' : 'User doesn\'t have permission to delete board /{}/.'.format(board.abbr)
+                }
+                return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
--- server/imageboard/priveleges.py
+++ server/imageboard/priveleges.py
@@ -24,7 +24,8 @@ EDIT_THREADS = 'edit_threads'
 DELETE_THREADS = 'delete_thread'
 
 GET_ADMIN_BOARDS = 'get_admin_boards'
+DELETE_BOARDS = 'delete_board'
 
 ADMIN_PRIVELEGES = MODER_PRIVELEGES + [
-    GET_ADMIN_BOARDS
+    GET_ADMIN_BOARDS, DELETE_BOARDS
 ]
