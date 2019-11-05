efbd48bac6db2b0628d6ef7b7454c48b85f82070
Lebed-kun <lebed-kun@mail.ru>
2019-11-05 14:03:29 +0400

    Implement get priveleged users handler

--- server/imageboard/controllers/admin/get_views.py
+++ server/imageboard/controllers/admin/get_views.py
@@ -10,6 +10,27 @@ from ...utils import get_visitor_ip, full_text_found
 from ..user.post_views import is_user_authorized
 from ... import priveleges
 
+# Helpers
+
+def get_priv_users_data(group, board, search=None):
+    data = []
+    
+    group = models.UserGroup.objects.filter(Q(name__icontains=group) & Q(board=board))[0]
+    priv_users = models.User.objects.filter(groups=group)
+    if search is not None and search.get('query', None) is not None:
+        priv_users = priv_users.filter(full_text_found(search['fields'], search['query']))
+
+    for user in priv_users:
+        data.append({
+            'name' : user.name,
+            'created_at' : user.created_at.strftime('%d/%m/%Y %H:%M:%S'),
+            'updated_at' : user.updated_at.strftime('%d/%m/%Y %H:%M:%S')
+        })
+
+    return data    
+
+# Views
+
 def get_admin_boards(request, *args, **kwargs):
     if request.method == 'GET':
         # Check if user is authorized
@@ -99,15 +120,31 @@ def get_priv_users(request, abbr, *args, **kwargs):
             return Response(message, status=status.HTTP_404_NOT_FOUND, content_type='application/json')
 
         # Success
+        group = request.data.get('group')
         priv_users = None
         search_query = request.query_params.get('q', None)
         search_fields = request.query_params.get('fields', 'name')
         search_fields = search_fields.split(',')
+
+        data = []
         if user_priveleges[0]['board'] is None:
-            # TO DO
-            pass
+            data = get_priv_users_data(group, board, {
+                'fields' : search_fields,
+                'query' : search_query
+            })          
         else:
-            # TO DO
-            pass
+            for priv in user_priveleges:
+                if priv['board'] == board:
+                    data = get_priv_users_data(group, board, {
+                        'fields' : search_fields,
+                        'query' : search_query
+                    })  
+            else:
+                message = {
+                    'message' : 'User doesn\'t have permission to get {}s from board /{}/.'.format(group.lower(), board.abbr)
+                }
+                return Response(message, status=status.HTTP_403_FORBIDDEN, content_type='application/json')
+
+        return Response(data, status=status.HTTP_200_OK, content_type='application/json')
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST, content_type='application/json')
