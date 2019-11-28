4a74912be2f93d4a8b678ab0d201150fd5497d04
Lebed-kun <lebed-kun@mail.ru>
2019-11-28 10:46:59 +0400

    Implement Menu component

--- web-frontend/components/Menu/Menu.jsx
+++ web-frontend/components/Menu/Menu.jsx
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Menu } from 'antd';
 
-import { BASE_REST_URL } from '../../constants.js';
+const SubMenu = Menu.SubMenu;
+const Item = Menu.Item;
 
 class LinkMenu extends Component {
-    state = {
-        homeLink : '/',
-        boardLinks : null
-    }
-    
-    getInitialProps() {
+    static HORIZONTAL = 'horizontal';
+    static VERTICAL = 'vertical';
+
+    renderLinks = (type, condition = props => true) => {
+        const links = this.props.links[type];
+        if (links && condition(this.props)) {
+            return (
+                <SubMenu key={type}>
+                    {links.map((el, id) => (
+                        <Item key={id}>
+                            <a href={el.href}>{el.title}</a>
+                        </Item>
+                    ))}
+                </SubMenu>
+            )
+        } else {
+            return null;
+        }
         
+    }
+
+    renderMenu = () => {
+        const boardLinks = this.props.links.boards;
+        if (boardLinks) {
+            return (
+                <Menu mode={this.props.position}>
+                    {this.renderLinks('prev', props => props.position === LinkMenu.HORIZONTAL)}
+                    {this.renderLinks('boards')}
+                    {this.renderLinks('next')}
+                </Menu>
+            )
+        } else {
+            return null;
+        }
     }
     
     render() {
-        const Element = this.props.component;
-
-        return (
-            <Element>
-
-            </Element>
-        )
+        return this.renderMenu();
     }
 }
+
+export default Menu;
