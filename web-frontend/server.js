2db980930d711b4ef3d56b8f882311326a920336
--unset <--unset>
2019-12-10 15:43:34 +0400

    Remove CORS from Node.js

--- web-frontend/server.js
+++ web-frontend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const next = require('next');
-const cors = require('cors');
 
 const port = +process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -9,8 +8,6 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
-
-    server.use(cors());
 
     server.get('/boards/:abbr', (req, res) => {
         return app.render(req, res, '/boards', { abbr : req.params.abbr });
