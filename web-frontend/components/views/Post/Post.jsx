c868af2d92427179b5d8bfc5e980944d4716ba10
Lebed-kun <lebed-kun@mail.ru>
2019-12-13 16:11:14 +0400

    Add Post and Thread

---
+++ web-frontend/components/views/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Card } from 'antd';
+
+class Post extends Component {
+    verticalCard = () => {
+        
+    }
+    
+    render() {
+        return null;
+    }
+}
